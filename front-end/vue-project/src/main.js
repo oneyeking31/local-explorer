369043e563ca2f55a55b98a111da24f4c8434713
oneyeking31 <57684415+oneyeking31@users.noreply.github.com>
2025-01-24 22:14:28 +0100

    Update main.js

--- front-end/vue-project/src/main.js
+++ front-end/vue-project/src/main.js
@@ -7,7 +7,7 @@ const app = createApp(App);
 // Initialize vue-google-maps
 app.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyCad5HsS1V6EbBp69iJHpT9PAAlr1Nmvkc', // Replace with your actual API key
+    key: process.env.GOOGLE_MAPS_API_KEY, // Replace with your actual API key
     libraries: 'places', // Optional: Add additional libraries if needed
   },
 });

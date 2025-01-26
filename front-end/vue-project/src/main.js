b39af8faa03ceccc73b4955e192ea8751a3b4561
oneyeking31 <57684415+oneyeking31@users.noreply.github.com>
2025-01-26 14:11:09 +0100

    Update main.js

--- front-end/vue-project/src/main.js
+++ front-end/vue-project/src/main.js
@@ -3,11 +3,10 @@ import App from './App.vue';
 import VueGoogleMaps from '@fawmi/vue-google-maps'; // Correct import
 
 const app = createApp(App);
-
 // Initialize vue-google-maps
 app.use(VueGoogleMaps, {
   load: {
-    key: process.env.GOOGLE_MAPS_API_KEY, // Replace with your actual API key
+    key: import.meta.env.VITE_API_KEY_GOOGLE_MAPS_API, // Replace with your actual API key
     libraries: 'places', // Optional: Add additional libraries if needed
   },
 });
